d65f56d2b2e8d29602a7397cdf80317cde9be6a1
PDogg66 <kunvtc6@gmail.com>
2020-05-05 13:48:53 +0700

    Updates

---
+++ precache-manifest.4a825ac0b15e24439e57616ae51b9de5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1d7ba1c1084725d0783cc3a1878866",
+    "url": "/mastercf/index.html"
+  },
+  {
+    "revision": "f86fa48921b4dcc09484",
+    "url": "/mastercf/static/css/main.745c6278.chunk.css"
+  },
+  {
+    "revision": "8966a893a88a5a7e203b",
+    "url": "/mastercf/static/js/2.f0730d9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mastercf/static/js/2.f0730d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86fa48921b4dcc09484",
+    "url": "/mastercf/static/js/main.1202168c.chunk.js"
+  },
+  {
+    "revision": "c52979e244389c29c692",
+    "url": "/mastercf/static/js/runtime-main.97a8f612.js"
+  },
+  {
+    "revision": "ecc9143b4b6c9ec07b2d96c86a523f6c",
+    "url": "/mastercf/static/media/backgr1.ecc9143b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mastercf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca65f7458465d17783025d583666b646",
+    "url": "/mastercf/static/media/ytbicon.ca65f745.png"
+  }
+]);
