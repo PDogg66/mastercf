434ddd02893de809e635dbc1f3084037e5a1a072
PDogg66 <60024893+PDogg66@users.noreply.github.com>
2020-05-05 22:33:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Home from "./home";
 import Product from "./Product";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
